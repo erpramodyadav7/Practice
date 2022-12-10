075af43479d0dbdc5b425b84ff95921f0c2d271d
pramod yadav <er.pramodyadav7@gmail.com>
2022-12-10 22:05:52 +0530

    main code

---
+++ BonusCal.py
@@ -0,0 +1,7 @@
+exp= int(input("Enter Work experience: "))
+sal= int(input("Enter your salary in thousands: "))
+bonus=(0.15*sal)
+if exp>=2 and sal>=25000:
+    print(f"Your bonus amount is Rs.{bonus}")
+else:
+    print(f"Sorry, you are not eligible for bonus")
